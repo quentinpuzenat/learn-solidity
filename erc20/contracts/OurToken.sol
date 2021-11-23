720307896fc0f725f4c8e0f9bd61e287f6192d65
Quentin Puzenat <qpuzenat@gmail.com>
2021-11-23 15:19:46 +0100

    created erc20 token

---
+++ erc20/contracts/OurToken.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract bigqToken is ERC20 {
+    constructor () ERC20("ExampleToken", "EGT") {
+        _mint(msg.sender, 10000 * 10 ** decimals());
+    }
+}
+
+
