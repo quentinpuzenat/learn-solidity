55a29df1ad36e86e18511a2ec761f453ee5566f6
Quentin Puzenat <qpuzenat@gmail.com>
2021-11-25 13:06:31 +0100

    updates

--- lottery/scripts/index.js
+++ lottery/scripts/index.js
@@ -1,5 +1,10 @@
 const { ethers } = require("hardhat");
+const { NonceManager } = require("@ethersproject/experimental");
 require("dotenv").config();
+
+const {
+  abi,
+} = require("/Users/quentin/github/learn-solidity/lottery/artifacts/contracts/Lottery.sol/Lottery.json");
 
 async function main() {
   // connect to provider, fuji testnet here
@@ -7,38 +12,76 @@ async function main() {
     "https://api.avax-test.network/ext/bc/C/rpc"
   );
 
-  let Lottery = await ethers.getContractFactory("Lottery");
-  let lottery = await Lottery.attach(
-    "0x7D51Fdd098e371248eBf07E0E22De0d3F0753248"
+  let lottery = new ethers.Contract(
+    "0x7D51Fdd098e371248eBf07E0E22De0d3F0753248",
+    abi,
+    connection
   );
 
   // create wallet from mnemonic
-  const wallet = await ethers.Wallet.fromMnemonic(
+  const wallet = new ethers.Wallet.fromMnemonic(
     "calm candy riot cash carpet wait route smart dice radar useful change"
   );
-
   const myWallet = new ethers.Wallet(process.env.PRIVATE_KEY);
 
-  let myWalletSigner = myWallet.connect(connection);
-  let walletSigner = wallet.connect(connection);
+  // signers creation
+  let signer = wallet.connect(connection);
+  let mysigner = myWallet.connect(connection);
 
-  const tx = {
-    from: wallet.address,
-    to: myWallet.address,
-    value: ethers.utils.parseUnits("0.1", "ether"),
-    gasPrice: connection.getGasPrice(),
-    nonce: connection.getTransactionCount(wallet.address, "latest"),
-    gasLimit: ethers.utils.hexlify(1000000),
-  };
+  let signerManager = new NonceManager(connection);
 
-  const transaction = await walletSigner.sendTransaction(tx);
-  console.log(transaction);
+  signerManager.incrementTransactionCount();
 
-  console.log(await walletSigner.getBalance("latest"));
-  console.log(await myWallet.address);
+  // const tx = {
+  //   from: signer.address,
+  //   to: mysigner.address,
+  //   value: ethers.utils.parseUnits("0.1", "ether"),
+  //   gasPrice: connection.getGasPrice(),
+  //   nonce: connection.getTransactionCount(wallet.address, "latest"),
+  //   gasLimit: ethers.utils.hexlify(1000000),
+  // };
+
+  // const transaction = await signer.sendTransaction(tx);
+  //console.log(transaction);
+
+  console.log(parseInt(await connection.getBalance(wallet.address)));
+
+  // start lottery
+  // let lotterysigned = lottery.connect(signer);
+  // console.log(
+  //   await lotterysigned.startLottery({
+  //     gasPrice: connection.getGasPrice(),
+  //     nonce: connection.getTransactionCount(wallet.address, "latest"),
+  //     gasLimit: ethers.utils.hexlify(1000000),
+  //   })
+  // );
+
+  // // enter lottery
+  // console.log(
+  //   await lotterysigned.enter({
+  //     gasPrice: connection.getGasPrice(),
+  //     nonce: connection.getTransactionCount(wallet.address, "latest"),
+  //     gasLimit: ethers.utils.hexlify(1000000),
+  //   })
+  // );
+
+  lotterysigned = lottery.connect(signer);
+
   console.log(
-    parseInt(await myWalletSigner.getBalance("latest"), 16) / 10 ** 18
+    await lotterysigned.enter({
+      value: lottery.getEntranceFee(),
+      gasPrice: signer.getGasPrice(),
+      nonce: connection.getTransactionCount(wallet.address, "latest"),
+      gasLimit: ethers.utils.hexlify(1000000),
+    })
   );
+
+  console.log((await lottery.players).length);
+  // console.log(await walletSigner.getBalance("latest"));
+  // console.log(await myWallet.address);
+  // console.log(
+  //   parseInt(await myWalletSigner.getBalance("latest"), 16) / 10 ** 18
+  // );
 
   // console.log(`Contract address: ${lottery.address}`);
   // console.log(`Random Wallet address: ${wallet.address}`);
