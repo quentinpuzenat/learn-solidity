0eb0acc73c259e063ad9b1946e57bc6fedfd3420
Quentin Puzenat <qpuzenat@gmail.com>
2021-11-28 18:50:37 +0100

    front infos processing and open sea link

--- epic-nfts/contracts/EpicNft.sol
+++ epic-nfts/contracts/EpicNft.sol
@@ -15,6 +15,8 @@ contract EpicNft is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    event NewEpicNFTMinted(address sender, uint256 tokenId);
+
     // our base svg code
     string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
@@ -93,5 +95,7 @@ contract EpicNft is ERC721URIStorage {
         console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
         // Increment the counter for when the next NFT is minted.
         _tokenIds.increment();
+
+        emit NewEpicNFTMinted(msg.sender, newItemId);
     }
 }
--- epic-nfts/epic-nfts-app/src/App.js
+++ epic-nfts/epic-nfts-app/src/App.js
@@ -3,14 +3,27 @@ import { ethers } from "ethers";
 import Account from "./components/Account";
 import EpicNft from "./utils/EpicNft.json";
 
+const CONTRACT_ADDRESS = "0xAD80e669A80D1aE07Add64a86e906e548F3a94a5";
+
 function App() {
   let [account, setAccount] = React.useState();
   let [balance, setBalance] = React.useState();
   let [provider, setProvider] = React.useState();
   let [signer, setSigner] = React.useState();
+  let [link, setLink] = React.useState();
+  let [loading, setLoading] = React.useState(false);
 
   const connectToMetamask = async () => {
     const { ethereum } = window;
+    let chainId = await ethereum.request({ method: "eth_chainId" });
+    console.log("Connected to chain " + chainId);
+
+    // String, hex code of the chainId of the Rinkebey test network
+    const rinkebyChainId = "0x4";
+    if (chainId !== rinkebyChainId) {
+      alert("You are not connected to the Rinkeby Test Network!");
+    }
+
     const accounts = await ethereum.request({ method: "eth_requestAccounts" });
     setAccount(accounts[0]);
 
@@ -20,11 +33,46 @@ function App() {
       setProvider(myProvider);
       setSigner(mySigner);
       setBalance((await mySigner.getBalance()) / 10 ** 18);
+
+      setupEventListener();
+    }
+  };
+
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          EpicNft.abi,
+          signer
+        );
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          setLink(
+            `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
+        });
+
+        console.log("Setup event listener!");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "0x1B7c7F22bf1F44d4172e207cDa235Ab12c6915Ba";
     const connectedContract = new ethers.Contract(
       CONTRACT_ADDRESS,
       EpicNft.abi,
@@ -33,7 +81,9 @@ function App() {
 
     console.log("Going to pop wallet now to pay gas...");
     let nftTxn = await connectedContract.makeAnEpicNFT();
-    nftTxn.wait();
+    setLoading(true);
+    await nftTxn.wait();
+    setLoading(false);
     console.log(
       `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
     );
@@ -41,13 +91,24 @@ function App() {
 
   return (
     <>
-      <div className="m-5">
+      <div className="m-5 h-screen">
         <div className="flex flex-row justify-between">
-          <div className="p-1 font-semibold">Mint NFTs</div>
-          <div className="shadow-lg">
+          <div className="p-1 font-semibold">
+            Mint NFTs{" "}
+            <button className="text-xs ml-2 px-2 py-1 bg-red-500 rounded-lg text-white">
+              <a
+                href="https://testnets.opensea.io/collection/demonslayer-v4"
+                target="_blank"
+                rel="noreferrer"
+              >
+                collection
+              </a>
+            </button>{" "}
+          </div>
+          <div>
             {!account ? (
               <button
-                className=" bg-blue-700 rounded-lg text-white px-2 py-1"
+                className="shadow-lg bg-blue-700 rounded-lg text-white px-2 py-1"
                 onClick={connectToMetamask}
               >
                 Connect your wallet
@@ -58,13 +119,32 @@ function App() {
           </div>
         </div>
 
-        {account && (
-          <button
-            className="bg-black text-white p-2 rounded-lg  hover:bg-blue-700 "
-            onClick={askContractToMintNft}
-          >
-            MINT
-          </button>
+        <div className="flex my-5">
+          {account && (
+            <button
+              className="bg-black text-white py-2 px-5 rounded-lg  hover:bg-blue-700 "
+              onClick={askContractToMintNft}
+            >
+              {!loading ? "MINT" : "PROCESSING ... "}
+            </button>
+          )}
+        </div>
+
+        {link && (
+          <div className="my-4">
+            <p>{link.slice(0, -97)}</p>
+            <br />
+            <div className="flex justify-center">
+              <a
+                className="bg-black hover:bg-blue-700 p-2 mt-4 text-white rounded-lg"
+                href={link.slice(-80)}
+                target="_blank"
+                rel="noreferrer"
+              >
+                Go to OpenSea
+              </a>
+            </div>
+          </div>
         )}
       </div>
     </>
